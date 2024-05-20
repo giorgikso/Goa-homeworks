ac47969730bf2a909e879938ff6c23a80259c0ad
giorgikso <giorgiksovreli201068@gmail.com>
2024-05-20 21:02:30 +0400

    commit

---
+++ day 01/lesson1.py
@@ -0,0 +1,23 @@
+name = "giorgi"
+
+
+surname = "ksovreli"
+
+
+
+
+
+
+name = "giorgi"
+age = 13
+height =(171.5)
+
+knows_programming = True
+is_Ugly = False
+
+isugly = False
+
+
+print (name + " " + surname)
+print (age)
+print (height)
